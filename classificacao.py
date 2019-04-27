aef63dfa65121314b5ff5a8cc352c7b06e5ee61e
hppod <hppodgornik@gmail.com>
2019-04-27 00:15:19 -0300

    implementado a taxa de acerto

--- classificacao.py
+++ classificacao.py
@@ -19,11 +19,33 @@ marcacoes = [1, 1, 1, -1, -1, -1]
 # elemento misterioso, será que ele é cachorro ou porco?
 misterioso = [1, 1, 1]
 misterioso2 = [1, 0, 0]
+misterioso3 = [0, 0, 1]
 
-# importando o algoritmo de treinamenot
-
+# importando o algoritmo de treinamento
 modelo = MultinomialNB()
-
 modelo.fit(dados, marcacoes)
-teste = [misterioso, misterioso2]
-print(modelo.predict(teste))
+
+testes = [misterioso, misterioso2, misterioso3]
+
+marcacoes_teste = [-1, 1, -1]
+
+resultado = modelo.predict(testes)
+print(resultado)
+
+diferencas = (resultado - marcacoes_teste)
+print(diferencas)
+
+acertos = [d for d in diferencas if d == 0]
+print(acertos)
+total_de_acertos = len(acertos)
+
+print(total_de_acertos)
+total_de_elementos = len(testes)
+print(total_de_elementos)
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+print(taxa_de_acerto)
+
+# 1 1 => 1-1=0
+# -1 -1 => -1 --1 = -1 + 1 = 0
+# -1 1 => -1 -1 = -2
+# 1 - 1 => 1 --1 = 1+ 1 = 2
