563f28d437b1a714e167ecb8ebe5c189f98a31bc
hppod <hppodgornik@gmail.com>
2019-04-27 09:37:33 -0300

    adicionando dados ao remoto

---
+++ dados.py
@@ -0,0 +1,22 @@
+import csv
+
+
+def carregar_acessos():
+    
+    x = []
+    y = []
+
+    arquivo = open('acesso.csv', 'r')
+    leitor = csv.reader(arquivo)
+    
+    # leitor.next()
+    
+    for home, como_funciona, contato, comprou in leitor:
+
+        dado = [int(home), int(como_funciona), int(contato)]
+        x.append(dado)
+        y.append(int(comprou))
+    
+    return x, y
+
+
