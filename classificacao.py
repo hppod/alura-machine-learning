c0d5becd68a129acc6120e9feaaf33920ca47ca3
hppod <hppodgornik@gmail.com>
2019-04-26 23:41:24 -0300

    definindo as variaveis

---
+++ classificacao.py
@@ -0,0 +1,10 @@
+# definindo o primeiro porco. Um array de carcteristicas do porco
+# seis elementos que conhecemos
+# 3 variaveis -> é gordinho? tem perna curta? faz au au?
+
+porco1 = [1, 1, 0]
+porco2 = [1, 1, 0]
+porco3 = [1, 1, 0]
+cachorro1 = [1, 1, 1]
+cachorro2 = [0, 1, 1]
+cachorro3 = [0, 1, 1]
