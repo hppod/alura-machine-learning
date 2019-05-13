415462bdbb1b8f0c7c1dc1dc5a317d0f7f5fc4d1
hppod <hppodgornik@gmail.com>
2019-05-12 22:22:03 -0300

    Mensagem de taxa de acerto

--- classifica_acessos.py
+++ classifica_acessos.py
@@ -10,14 +10,16 @@ teste_dados = x[-9:]
 teste_marcacoes = y[-9:]
 
 modelo = MultinomialNB()
-modelo.fit(x, y)
+modelo.fit(treino_dados, treino_marcacoes)
 
-resultado = modelo.predict(x)
-diferencas = resultado - y
+resultado = modelo.predict(teste_dados)
+diferencas = resultado - teste_marcacoes
+
 acertos = [d for d in diferencas if d == 0]
+
 total_de_acertos = len(acertos)
-total_de_elementos = len(x)
+total_de_elementos = len(teste_dados)
 taxa_de_acertos = 100.0 * total_de_acertos / total_de_elementos
 
-print(taxa_de_acertos)
-print(total_de_elementos)
+print('Taxa de acerto:', taxa_de_acertos)
+print('Total de elementos testados:', total_de_elementos)
