6b82fa03da79fc806a3a888efa6c5d61a1b2086a
hppod <hppodgornik@gmail.com>
2019-05-12 21:48:18 -0300

    Arrumando o erro de next não existe

--- dados.py
+++ dados.py
@@ -2,21 +2,19 @@ import csv
 
 
 def carregar_acessos():
-    
+
     x = []
     y = []
 
     arquivo = open('acesso.csv', 'r')
     leitor = csv.reader(arquivo)
-    
-    # leitor.next()
-    
+
+    next(leitor)
+
     for home, como_funciona, contato, comprou in leitor:
 
         dado = [int(home), int(como_funciona), int(contato)]
         x.append(dado)
         y.append(int(comprou))
-    
+
     return x, y
-
-
