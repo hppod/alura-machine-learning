7bd860046fade5e58e119d2a82cf34efe4b492eb
hppod <hppodgornik@gmail.com>
2019-05-13 22:54:34 -0300

    Implementando a função que carrega o csv e lendo a var y

---
+++ classifica_buscas.py
@@ -0,0 +1,4 @@
+from dados import carregar_buscas
+x, y = carregar_buscas()
+
+print(y)
--- dados.py
+++ dados.py
@@ -18,3 +18,21 @@ def carregar_acessos():
         y.append(int(comprou))
 
     return x, y
+
+
+def carregar_buscas():
+
+    x = []
+    y = []
+
+    arquivo = open('busca.csv', 'r')
+
+    leitor = csv.reader(arquivo)
+    next(leitor)
+
+    for home, busca, logado, comprou in leitor:
+        dado = [int(home), busca, int(logado)]
+        x.append(dado)
+        y.append(int(comprou))
+
+    return x, y
