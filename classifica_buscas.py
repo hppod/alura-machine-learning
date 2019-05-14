1a0581851cbf215ac21218192d7be6bb23e3a612
hppod <hppodgornik@gmail.com>
2019-05-13 23:23:28 -0300

    Implementando o treino do modelo

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -1,3 +1,4 @@
+from sklearn.naive_bayes import MultinomialNB
 import pandas as pd
 
 df = pd.read_csv('busca.csv')
@@ -11,7 +12,9 @@ yDummies_df = y_df
 x = xDummies_df.values
 y = yDummies_df.values
 
-tamanho_de_treino = 0.9 * len(y)
+porcentagem_de_treino = 0.9
+
+tamanho_de_treino = int(porcentagem_de_treino * len(y))
 
 treino_dados = x[:tamanho_de_treino]
 treino_marcacoes = y[:tamanho_de_treino]
@@ -20,3 +23,17 @@ tamanho_de_teste = len(y) - tamanho_de_treino
 
 teste_dados = x[-tamanho_de_teste:]
 teste_marcacoes = y[-tamanho_de_teste:]
+
+modelo = MultinomialNB()
+modelo.fit(treino_dados, treino_marcacoes)
+
+resultado = modelo.predict(teste_dados)
+diferencas = resultado - teste_marcacoes
+
+acertos = [d for d in diferencas if d == 0]
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste_dados)
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+print(taxa_de_acerto)
+print(total_de_elementos)
