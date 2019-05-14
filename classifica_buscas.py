aa1e2e97eecbaa33b3563e78aafe4196e8919f4a
hppod <hppodgornik@gmail.com>
2019-05-13 23:16:06 -0300

    Definindo o tamanho de treino e o tamanho de teste

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -11,3 +11,12 @@ yDummies_df = y_df
 x = xDummies_df.values
 y = yDummies_df.values
 
+tamanho_de_treino = 0.9 * len(y)
+
+treino_dados = x[:tamanho_de_treino]
+treino_marcacoes = y[:tamanho_de_treino]
+
+tamanho_de_teste = len(y) - tamanho_de_treino
+
+teste_dados = x[-tamanho_de_teste:]
+teste_marcacoes = y[-tamanho_de_teste:]
